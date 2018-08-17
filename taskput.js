253aed8e8cad5780a041415d83cae5699667dae6
Shefali33 <shefalibhasin3@gmail.com>
2018-08-17 18:12:15 +0530

    mymethodput

--- taskput.js
+++ taskput.js
@@ -20,14 +20,26 @@ export function updateRecord(){
             else{
                 let value = contents.split(/[\n\r]+/g);
                 let newArr;
+                // value.forEach(function(element){
+                //     newArr = element.split(',')
+                //   if(newArr[0] == id){
+                //       newArr[1] = data1[1];
+                //       value.splice(0,1,`${newArr}\n\r`);
+                //   }
+                //   console.log(value);
+                //   writeFile('newtask1.txt',`${value}`,function(err){
+                //             if (err) throw err;
+                //             res.end("Username Updated...")
+                //         })
+                // })
                 for(let i=0;i<value.length;i++){
                     newArr = value[i].split(',');
                         if(id == newArr[0]){
                         newArr[1] = data1[1];
-                        value.splice(i,1,newArr.join(','));
+                        value.splice(i,1,`${newArr}\n\r`);
                         }   
-                        console.log(value)
                     }
+                    console.log(value);
                     writeFile('newtask1.txt',`${value}`,function(err){
                         if (err) throw err;
                         res.send("Username Updated...")
